13dee499f1f7ba133f3911a5b3247b63729c0f07
Rendy <rendyexpayer05@gmail.com>
2026-01-27 09:58:30 +0800

    feat: add music player UI and basic play/pause logic

--- src/data/songs.js
+++ src/data/songs.js
@@ -1,5 +1,6 @@
 import rainyDay from "../assets/audio/lofi-rainy-day.mp3";
 import studyTimer from "../assets/audio/lofi-study-timer.mp3";
+import jazzMusic from "../assets/audio/lofi-jazz-1.mp3";
 
 export const PLAYLIST = [
   {
@@ -9,9 +10,15 @@ export const PLAYLIST = [
     src: rainyDay,
   },
   {
-    id: 1,
+    id: 2,
     title: "Lofi Study timer",
     artist: "Lofi Girl",
     src: studyTimer,
+  },
+  {
+    id: 3,
+    title: "Lofi Jazz",
+    artist: "Lofi Music",
+    src: jazzMusic,
   },
 ];
