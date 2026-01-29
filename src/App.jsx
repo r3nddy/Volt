c21d06b09863fff0130aa9caef1f42d789485236
Rendy <rendyexpayer05@gmail.com>
2026-01-29 21:46:27 +0800

    feat: add Clock component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import ArchCard from "./components/ArchCard";
+import Clock from "./components/Clock";
 import MusicPlayer from "./components/MusicPlayer";
 import WeatherCard from "./components/WeatherCard";
 
@@ -6,10 +7,15 @@ function App() {
   return (
     <div className="min-h-screen bg-bg-dark text-white p-8 flex items-center justify-center font-sans selection:bg-purple-500/30">
       <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6 w-full max-w-7xl h-full md:h-[80vh] max-h-225">
+        {/* kolom kiri */}
         <div className="flex flex-col gap-6 lg:col-span-1 h-full">
           <WeatherCard />
           <ArchCard />
           <MusicPlayer />
+        </div>
+        {/* kolom tengah */}
+        <div className="flex flex-col gap-6 lg:col-span-2 h-full">
+          <Clock />
         </div>
       </div>
     </div>
---
+++ src/components/Clock.jsx
@@ -0,0 +1,62 @@
+import { ArrowRight, Lock } from "lucide-react";
+
+const Clock = () => {
+  const time = "01.03";
+  const periond = "AM";
+  const date = "Friday, 05 April 2004";
+
+  return (
+    <div className="bg-card-dark h-full flex flex-col items-center justify-center relative p-8">
+      <div className="flex flex-col items-center mb-12">
+        <div className="flex items-baseline gap-4 mb-2">
+          <h1 className="text-8xl font-bold text-white tracking-tight">
+            {time}
+          </h1>
+          <span className="text-4xl font-semibold text-purple-400">
+            {periond}
+          </span>
+        </div>
+        <p className="text-2xl text-purple-200/80 font-medium">{date}</p>
+      </div>
+
+      <div className="relative mb-12 group">
+        <div className="w-48 h-48 rounded-full bg-linear-to-tr from-pink-400 to-purple-600 p-1 shadow-[0_0_50px_rgba(192,132,252,0.3)]">
+          <div className="w-full h-full rounded-full bg-card-dark overflow-hidden">
+            {/* tempat foto profil */}
+            <div
+              className="w-full h-full bg-cover bg-center scale-110"
+              style={{
+                backgroundImage:
+                  "url('https://c4.wallpaperflare.com/wallpaper/236/691/651/anime-girls-sword-anime-warrior-wallpaper-preview.jpg')",
+              }}
+            ></div>
+            <div className="w-full h-full flex items-center justify-center bg-gray-800 text-6xl">
+              👓
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* slider */}
+      <div className="w-full max-w-sm h-14 bg-card-dark rounded-full flex items-center justify-between px-2 relative group cursor-pointer hover:bg-[#202025] transition-colors shadow-lg">
+        <div className="w-10 h-10 rounded-full flex items-center justify-center text-gray-500">
+          <Lock size={18} />
+        </div>
+
+        <div className="flex gap-2 opacity-50">
+          <div className="w-2 h-2 rounded-full bg-gray-600"></div>
+          <div className="w-2 h-2 rounded-full bg-gray-600"></div>
+          <div className="w-2 h-2 rounded-full bg-gray-600"></div>
+          <div className="w-2 h-2 rounded-full bg-white"></div>
+          <div className="w-2 h-2 rounded-full bg-white"></div>
+        </div>
+
+        <div className="w-10 h-10 rounded-full bg-purple-400 flex items-center justify-center text-black shadow-[0_0_15px_rgba(192,132,252,0.5)]">
+          <ArrowRight size={20} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Clock;
