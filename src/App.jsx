8bad5581c02ad088225e32ebe4853d602d5c7c0b
Rendy <rendyexpayer05@gmail.com>
2026-09-02 18:44:02 +0800

    fix: adjust alignment of main container in App component

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
   const weather = useWeather(city);
 
   return (
-    <div className="min-h-screen bg-bg-dark text-white p-8 flex items-center justify-center font-sans selection:bg-purple-500/30">
+    <div className="min-h-screen bg-bg-dark text-white p-8 flex items-start justify-center font-sans selection:bg-purple-500/30">
       <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6 w-full max-w-7xl h-full md:h-[80vh] max-h-225">
         {/* kolom kiri */}
         <div className="flex flex-col gap-6 lg:col-span-1 h-full">
